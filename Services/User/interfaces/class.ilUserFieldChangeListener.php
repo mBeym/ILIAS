491cb6eccb701932c12bb203440c6209cfee8640
Marvin Beym <mBeym@databay.de>
2021-09-21 13:12:19 +0200

    Add UserFieldChangeListener abstract class

---
+++ Services/User/interfaces/class.ilUserFieldChangeListener.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+/* Copyright (c) 1998-2020 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+namespace ILIAS\Services\User;
+
+use ILIAS\DI\Container;
+
+abstract class ilUserFieldChangeListener
+{
+    private Container $dic;
+
+    public function __construct(Container $dic) {
+        $this->dic = $dic;
+    }
+
+    /**
+     * Should return a description for a user profile field.
+     * Returning null or an empty string will skip the listener
+     * @param string $fieldName
+     * @return string|null
+     */
+    abstract public function getDescriptionForField(string $fieldName) : ?string;
+
+    /**
+     * Should return the component name like it would be used to raise an event
+     * @example "Services/Mail"
+     * @return string
+     */
+    abstract public function getComponentName() : string;
+}
