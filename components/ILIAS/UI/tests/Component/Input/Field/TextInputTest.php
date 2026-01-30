1cda4529e46c0431d62bc29b92ceb3f29e6392c2
Marvin Beym <mBeym@databay.de>
2026-01-30 12:48:14 +0100

    Implement autocomplet for ks form text input field

--- components/ILIAS/UI/tests/Component/Input/Field/TextInputTest.php
+++ components/ILIAS/UI/tests/Component/Input/Field/TextInputTest.php
@@ -28,6 +28,8 @@ use ILIAS\UI\Implementation\Component\SignalGenerator;
 use ILIAS\UI\Component\Input\Field;
 use ILIAS\Data;
 use ILIAS\Refinery\Factory as Refinery;
+use ILIAS\UI\URLBuilder;
+use ILIAS\UI\URLBuilderToken;
 
 class TextInputTest extends ILIAS_UI_TestBase
 {
@@ -167,5 +169,43 @@ class TextInputTest extends ILIAS_UI_TestBase
 
         $content = $text->getContent();
         $this->assertEquals("<script>alert()</script>", $content->value());
+    }
+
+    public function testTextWithAutocompleteEndpoint(): void
+    {
+        $url_builder = new URLBuilder(new Data\URI('http://wwww.ilias.de?ref_id=1'));
+        $token = new URLBuilderToken(['t'], 't');
+        $f = $this->getFieldFactory();
+        $text = $f->text('my_text');
+
+        $this->assertEquals(null, $text->getAsyncAutocompleteEndpoint());
+        $this->assertEquals(null, $text->getAsyncAutocompleteToken());
+
+        $text = $text->withAsyncAutocomplete(
+            $url_builder,
+            $token
+        );
+        $this->assertEquals($url_builder, $text->getAsyncAutocompleteEndpoint());
+        $this->assertEquals($token, $text->getAsyncAutocompleteToken());
+    }
+
+    public function testTextWithAutocompleteEndpointJSAdded(): void
+    {
+        $token = $this->createMock(URLBuilderToken::class);
+        $token->expects($this->once())
+            ->method('render');
+        $url_builder = $this->createMock(URLBuilder::class);
+        $url_builder->expects($this->once())
+            ->method('renderObject')
+            ->with([$token]);
+
+        $f = $this->getFieldFactory();
+        $text = $f->text('my_text')->withAsyncAutocomplete(
+            $url_builder,
+            $token
+        );
+
+        $renderer = $this->getDefaultRenderer();
+        $renderer->render($text);
     }
 }
