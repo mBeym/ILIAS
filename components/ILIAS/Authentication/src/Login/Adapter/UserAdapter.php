93926c7f90aa76899c2b690626e774992343960c
Marvin Beym <mBeym@databay.de>
2026-07-24 13:08:50 +0200

    Use usr_auth_data table in UserAdapter

--- components/ILIAS/Authentication/src/Login/Adapter/UserAdapter.php
+++ components/ILIAS/Authentication/src/Login/Adapter/UserAdapter.php
@@ -20,11 +20,14 @@ declare(strict_types=1);
 
 namespace ILIAS\Authentication\Login\Adapter;
 
+use ilDBConstants;
+use ilDBInterface;
 use ILIAS\Authentication\Login\Port\Lockout\LoginAttemptRepository;
 use ILIAS\Authentication\Login\Port\Account\LoginTimestampsRepository;
 use ILIAS\Authentication\Login\Port\Password\PasswordChangeTrackingRepository;
 use ILIAS\Authentication\Login\Port\Lockout\AccountDeactivation;
 use ILIAS\Authentication\Login\UserId;
+use ilUtil;
 
 final readonly class UserAdapter implements
     LoginAttemptRepository,
@@ -32,49 +35,66 @@ final readonly class UserAdapter implements
     PasswordChangeTrackingRepository,
     AccountDeactivation
 {
-    private \ilDBInterface $db;
+    public const string USER_AUTH_DATA_TABLE_NAME = 'usr_auth_data';
 
-    public function __construct(\ilDBInterface $db)
+    private ilDBInterface $db;
+
+    public function __construct(ilDBInterface $db)
     {
         $this->db = $db;
     }
 
     public function getCount(UserId $user_id): int
     {
-        $query = 'SELECT login_attempts FROM usr_data WHERE usr_id = %s';
-        $result = $this->db->queryF($query, ['integer'], [$user_id->value()]);
+        $result = $this->db->queryF('SELECT login_attempts FROM ' . self::USER_AUTH_DATA_TABLE_NAME . ' WHERE usr_id = %s',
+            [ilDBConstants::T_INTEGER],
+            [$user_id->value()]
+        );
         $record = $this->db->fetchAssoc($result);
         return (int) ($record['login_attempts'] ?? 0);
     }
 
     public function increment(UserId $user_id): void
     {
-        $query = 'UPDATE usr_data SET login_attempts = (login_attempts + 1) WHERE usr_id = %s';
-        $this->db->manipulateF($query, ['integer'], [$user_id->value()]);
+        $this->db->manipulateF('UPDATE ' . self::USER_AUTH_DATA_TABLE_NAME . ' SET login_attempts = (login_attempts + 1) WHERE usr_id = %s',
+            [ilDBConstants::T_INTEGER],
+            [$user_id->value()]
+        );
     }
 
     public function reset(UserId $user_id): void
     {
-        $query = 'UPDATE usr_data SET login_attempts = 0 WHERE usr_id = %s';
-        $this->db->manipulateF($query, ['integer'], [$user_id->value()]);
+        $this->db->manipulateF(
+            'UPDATE ' . self::USER_AUTH_DATA_TABLE_NAME . ' SET login_attempts = 0 WHERE usr_id = %s',
+            [ilDBConstants::T_INTEGER],
+            [$user_id->value()]
+        );
     }
 
     public function refreshLogin(UserId $user_id): void
     {
-        $now = \ilUtil::now();
-        $query = 'UPDATE usr_data SET last_login = %s WHERE usr_id = %s';
-        $this->db->manipulateF($query, ['timestamp', 'integer'], [$now, $user_id->value()]);
+        $this->db->manipulateF(
+            'UPDATE ' . self::USER_AUTH_DATA_TABLE_NAME . ' SET last_login = %s WHERE usr_id = %s',
+            [ilDBConstants::T_INTEGER, ilDBConstants::T_INTEGER],
+            [time(), $user_id->value()]
+        );
     }
 
     public function resetLastChange(UserId $user_id): void
     {
-        $query = 'UPDATE usr_data SET last_password_change = 0 WHERE usr_id = %s';
-        $this->db->manipulateF($query, ['integer'], [$user_id->value()]);
+        $this->db->manipulateF(
+            'UPDATE ' . self::USER_AUTH_DATA_TABLE_NAME . ' SET last_password_change = 0 WHERE usr_id = %s',
+            [ilDBConstants::T_INTEGER],
+            [$user_id->value()]
+        );
     }
 
     public function deactivate(UserId $user_id): void
     {
-        $query = 'UPDATE usr_data SET active = 0, inactivation_date = %s WHERE usr_id = %s';
-        $this->db->manipulateF($query, ['timestamp', 'integer'], [\ilUtil::now(), $user_id->value()]);
+        $this->db->manipulateF(
+            'UPDATE usr_data SET active = 0, inactivation_date = %s WHERE usr_id = %s',
+            ['timestamp', 'integer'],
+            [ilUtil::now(), $user_id->value()]
+        );
     }
 }
